b7caa7f34cbc5582a1484383f7d6acdd391d9f5e
prstephens <pr.stephens@gmail.com>
2021-03-10 13:44:00 +0000

    changes

--- install.sh
+++ install.sh
@@ -19,13 +19,13 @@ MIRRORLIST_URL="https://archlinux.org/mirrorlist/?country=GB&protocol=https&use_
 #  cfdisk $device
 #fi 
 
+pacman -Sy --noconfirm pacman-contrib dialog
+
 if [[ $(ping -q -w1 -c1 google.com &>/dev/null && echo online || echo offline) == "offline" ]]; 
     then
         dialog --backtitle "$BACKTITLE" --title 'Installation Stopped' --msgbox 'No Interent connection' 0 0
         exit
 fi
-
-pacman -Sy --noconfirm pacman-contrib dialog
 
 echo "Updating mirror list"
 curl -sL "$MIRRORLIST_URL" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
@@ -80,7 +80,7 @@ performInstall()
     # Install Arch Linux
     echo "Starting install.."
     echo "Installing Arch Linux with Zen kernel, rEFInd as bootloader" 
-    pacstrap /mnt base base-devel networkmanager dnsutils reflector linux linux-firmware refind efibootmgr intel-ucode ntfs-3g xorg xorg-server xorg-xinit nano nano-syntax-highlighting sudo git nvidia nvidia-settings bluez bluez-utils pulseaudio rxvt-unicode wget dialog cups hplip ufw gufw archlinux-keyring anything-sync-daemon mtpfs gvfs-mtp gvfs-gphoto2 gvfs-smb openssh openvpn
+    pacstrap /mnt base base-devel networkmanager dnsutils reflector linux linux-firmware refind efibootmgr intel-ucode ntfs-3g xorg xorg-server xorg-xinit nano nano-syntax-highlighting sudo git nvidia nvidia-settings bluez bluez-utils pulseaudio rxvt-unicode wget dialog cups hplip ufw gufw archlinux-keyring anything-sync-daemon mtpfs gvfs-mtp gvfs-gphoto2 gvfs-smb openssh openvpn ncdu
 
     # Generate fstab
     genfstab -U /mnt >> /mnt/etc/fstab
@@ -181,8 +181,7 @@ EOT
     arch-chroot /mnt curl https://raw.githubusercontent.com/prstephens/archinstallscript/master/sweet/issue -o /etc/issue
 
     arch-chroot /mnt chown -R $user:$user /home/$user/.config
-    arch-chroot /mnt chown $user:$user /home/$user/.Xresources
-
+    
     # Get yay ready 
     echo "Getting yay all ready for ${user}..."
     arch-chroot /mnt git clone https://aur.archlinux.org/yay.git /home/$user/yay
